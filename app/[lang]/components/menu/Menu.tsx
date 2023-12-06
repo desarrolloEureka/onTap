57d8a95c914304122c64cedb1bca63f597f9fb04
Cristian <cristian@eurekadreams.com>
2023-12-06 09:59:29 -0500

    feat: Merge with main

--- app/[lang]/components/menu/Menu.tsx
+++ app/[lang]/components/menu/Menu.tsx
@@ -1,137 +1,62 @@
-import { MenuProps } from '@/types/menu';
-import Business from '@mui/icons-material/Business';
-import Home from '@mui/icons-material/Home';
-import People from '@mui/icons-material/People';
-import Work from '@mui/icons-material/Work';
+import { MenuProps } from "@/types/menu";
+import Business from "@mui/icons-material/Business";
+import Home from "@mui/icons-material/Home";
+import People from "@mui/icons-material/People";
+import Work from "@mui/icons-material/Work";
 import {
   IconButton,
   ListItemText,
   MenuItem,
   Paper,
   Toolbar,
-} from '@mui/material';
-import Image from 'next/image';
+} from "@mui/material";
+import Image from "next/image";
 
 const Menu = ({ dictionary }: MenuProps) => {
   return (
-    <Paper sx={{ width: '100%', backgroundColor: '#62AD9B' }}>
-      <Toolbar
-        sx={{
-          margin: 'auto',
-          display: 'flex',
-          justifyContent: 'center',
-          width: '1440px',
-          height: '139px',
-        }}
-      >
+    <Paper 
+     sx={{ width: "100%", backgroundColor: "#62AD9B" }}>
+      <Toolbar className=" flex tw-m-auto tw-justify-center tw-h-[139px] tw-w-[1440px]">
         <Image
-          src='/images/capa_1.png'
-          alt='Logo One Tap'
-          width='81'
-          height='77'
-          style={{
-            position: 'absolute',
-            left: '20px',
-            top: '50%',
-            transform: 'translateY(-50%)',
-          }}
+          className=" tw-absolute tw-left-5 tw-bottom-16 tw-translate-y-1/2"
+          src="/images/capa_1.png"
+          alt="Logo One Tap"
+          width="81"
+          height="77"
         />
 
-        <MenuItem
-          sx={{
-            width: '200px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginLeft: '-250px',
-          }}
-        >
-          <IconButton sx={{ fontSize: '24px' }}>
+        <MenuItem className="tw-flex tw-w-[200px] tw-flex-col tw-items-center tw-ml-[-250px] ">
+          <IconButton className="tw-text-[24px] ">
             <Home />
           </IconButton>
-          <ListItemText
-            sx={{
-              fontSize: '14px',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-              color: 'white',
-            }}
-          >
+          <ListItemText className=" tw-text-[14px] tw-overflow-hidden tw-overflow-ellipsis tw-whitespace-nowrap tw-text-white ">
             {dictionary.mainTab.tab1}
           </ListItemText>
         </MenuItem>
 
-        <MenuItem
-          sx={{
-            width: '200px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <IconButton sx={{ fontSize: '24px' }}>
-            <People />
+        <MenuItem className="tw-flex tw-w-[200px] tw-flex-col tw-items-center ">
+          <IconButton className="tw-text-[24px] ">
+            <Home />
           </IconButton>
-          <ListItemText
-            sx={{
-              fontSize: '14px',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-              color: 'white',
-            }}
-          >
+          <ListItemText className=" tw-text-[14px] tw-overflow-hidden tw-overflow-ellipsis tw-whitespace-nowrap tw-text-white ">
             {dictionary.mainTab.tab2}
           </ListItemText>
         </MenuItem>
 
-        <MenuItem
-          sx={{
-            width: '200px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <IconButton sx={{ fontSize: '24px' }}>
-            <Work />
+        <MenuItem className="tw-flex tw-w-[200px] tw-flex-col tw-items-center ">
+          <IconButton className="tw-text-[24px] ">
+            <Home />
           </IconButton>
-          <ListItemText
-            sx={{
-              fontSize: '14px',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-              color: 'white',
-            }}
-          >
+          <ListItemText className=" tw-text-[14px] tw-overflow-hidden tw-overflow-ellipsis tw-whitespace-nowrap tw-text-white ">
             {dictionary.mainTab.tab3}
           </ListItemText>
         </MenuItem>
 
-        <MenuItem
-          sx={{
-            width: '200px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <IconButton sx={{ fontSize: '24px' }}>
-            <Business />
+        <MenuItem className="tw-flex tw-w-[200px] tw-flex-col tw-items-center ">
+          <IconButton className="tw-text-[24px] ">
+            <Home />
           </IconButton>
-          <ListItemText
-            sx={{
-              fontSize: '14px',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-              color: 'white',
-              maxWidth: '100px',
-              textAlign: 'center',
-            }}
-          >
+          <ListItemText className=" tw-text-[14px] tw-overflow-hidden tw-overflow-ellipsis tw-whitespace-nowrap tw-text-white ">
             {dictionary.mainTab.tab4}
           </ListItemText>
         </MenuItem>
--- app/[lang]/types/dictionary.ts
+++ app/[lang]/types/dictionary.ts
@@ -6,4 +6,8 @@ export interface Dictionary {
   mainTab: { tab1: string; tab2: string; tab3: string; tab4: string };
   loginView: { username: string; password: string; login: string };
   homeView: { views: string; title: string; };
+  recoverPassword: {recoverpassword:string; mail:string; next:string;};
+  recoveryCode:{nextCode:string; resendcode:string; code:string; enter_the_recovery_code_that_we_have_sent_to_your_email:string; recoverycode:string; }
+  newPassword:{createnewpassword:string; newpassword:string; repeatpassword:string; nextnewpassword:string}
+  passwordChangedSuccessfully:string;
 }
