87a92508f078ee50444588a9f8716f35811705fa
Cristian <cristian@eurekadreams.com>
2023-12-06 10:05:00 -0500

    bugfix: recovery login container

--- app/[lang]/components/login/Login.tsx
+++ app/[lang]/components/login/Login.tsx
@@ -12,7 +12,54 @@ const Login = ({ dictionary }: LoginProps) => {
    
     <>
       
-
+ <div className='flex tw-flex-col tw-justify-items-center tw-h-max tw-justify-center tw-bg-[url("/images/loginBackground.png")] tw-bg-no-repeat tw-bg-center tw-bg-cover'>
+      <Menu dictionary={dictionary} />
+      <Container className='tw-bg-primary tw-shadow-md tw-rounded-2xl tw-h-[618px] tw-w-[794px] flex tw-justify-center  tw-justify-items-center tw-text-center '>
+        <Box className='tw-w-full tw-flex tw-justify-center tw-justify-items-center '>
+          <Image
+            className='tw-mt-14'
+            src='/images/logo_onetap_blanco.png'
+            alt='Logo'
+            width={190}
+            height={160}
+          />
+        </Box>
+        <TextField
+          className='tw-h-[52px] tw-w-[386px] tw-mt-14 tw-text-sm  '
+          required
+          id='outlined-required'
+          label={dictionary.loginView.username}
+          defaultValue=''
+          variant='outlined'
+          InputProps={{ className: 'tw-rounded-3xl' }}
+        />
+        <Typography
+          className='tw-text-white tw-mt-3 tw-mr-80'
+          variant='body2'
+          color='textSecondary'
+        >
+          {dictionary.loginView.username}
+        </Typography>
+        <TextField
+          className='tw-h-[52px] tw-w-[386px] tw-mt-14 tw-text-sm  '
+          required
+          id='outlined-password'
+          label={dictionary.loginView.password}
+          type='password'
+          defaultValue=''
+          variant='outlined'
+          InputProps={{ className: 'tw-rounded-3xl' }}
+        />
+        <Typography
+          className='tw-text-white tw-mt-3 tw-mr-72'
+          variant='body2'
+          color='textSecondary'
+        >
+          {dictionary.loginView.password}
+        </Typography>
+        <Button className='tw-text-white '>{dictionary.loginView.login}</Button>
+      </Container>
+    </div>
       <div>
         <div className='flex tw-flex-col tw-justify-items-center tw-h-max tw-justify-center tw-bg-[url("/images/loginBackground.png")] tw-bg-no-repeat tw-bg-center tw-bg-cover'>
         <Menu dictionary={dictionary} />
