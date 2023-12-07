1c4faefbd476daab57160b80403214a4b4e0f9d9
rodacapera <rodacapera@gmail.com>
2023-12-07 08:23:27 -0500

    feat: clean code

--- app/[lang]/views/home/hook/homeHook.tsx
+++ app/[lang]/views/home/hook/homeHook.tsx
@@ -1,18 +1,10 @@
-import { GetLoginQuery, GetUser } from '@/reactQuery/users';
+import { GetUser } from '@/reactQuery/users';
 import { User } from '@/types/user';
 import { useEffect, useState } from 'react';
 
 const useHomeHook = () => {
   const [user, setUser] = useState<User[]>([]);
   const { isLoading, data, error } = GetUser();
-  // const { isLoading, data, error } = GetLoginQuery({
-  //   user: 'rodacapera',
-  //   password: 'Rhonald870729',
-  // });
-
-  // const handleUsers = (data: any) => {
-  //   setUser((prev: any) => [...prev, data]);
-  // };
 
   useEffect(() => {
     data && user.length == 0 && setUser((prev: any) => [...prev, data]);
