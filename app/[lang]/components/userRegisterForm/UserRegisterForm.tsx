69d7ef049f00609b1dca2562b1fc5bc34c71c395
camilo-a-g <cagarciaz@correo.udistrital.edu.co>
2024-02-08 09:55:50 -0500

    Se crea formulario para registrar cliente

--- app/[lang]/components/userRegisterForm/UserRegisterForm.tsx
+++ app/[lang]/components/userRegisterForm/UserRegisterForm.tsx
@@ -17,35 +17,50 @@ import {
     Container,
     TextField,
     Typography,
+    Select
 } from '@mui/material';
 import { Dictionary } from "@/types/dictionary";
 import UserRegisterForm from "./hooks/UserRegisterForm";
+import MenuItem from '@mui/material/MenuItem';
 
 
-const UserRegister = (dictionary: Dictionary, isBasicPlan: Boolean) => {
+const UserRegister = () => {
     const {
         dni,
         email,
         name,
         lastName,
+        plan,
         setDni,
         setEmail,
         setName,
-        setLastName
-    } = UserRegisterForm(dictionary, isBasicPlan);
+        setLastName,
+        setPlan, //es un dropdown con valor "basic" por defecto y otra opcion "profesional"
+        dataRegisterHandle,
+        errorMailForm,
+        errorDataForm
+    } = UserRegisterForm();
 
     return (
         <div className='tw-flex tw-h-screen tw-items-center tw-justify-center tw-bg-[url("/images/loginBackground.png")] tw-bg-no-repeat tw-bg-center tw-bg-cover'>
-            <Container className='tw-bg-[#02AF9B] tw-shadow-m  tw-rounded-2xl tw-h-[618px] tw-w-[794px] tw-flex tw-flex-col tw-justify-center tw-items-center '>
+            <Container className='tw-bg-[#02AF9B] tw-shadow-m  tw-rounded-2xl tw-h-[700px] tw-w-[794px] tw-flex tw-flex-col tw-justify-center tw-items-center '>
                 <>
-                    <Box className='tw-w-[386px]'>
+                    <Box className='tw-w-[386px]'component="form">
+                        <Typography
+                            className='tw-text-white  tw-mt-10 tw-w-full'
+                            variant='body2'
+                            color='textPrimary'
+                            display={'flow'}
+                        >
+                            Registro de usuario
+                        </Typography>
                         <Typography
                             className='tw-text-white  tw-mt-10 tw-w-full'
                             variant='body2'
                             color='textSecondary'
                             display={'flow'}
                         >
-                            DNI:{' '}
+                            Cedula:{' '}
                         </Typography>
                         <TextField
                             className='tw-h-[52px] tw-mt-1 tw-w-[386px]  tw-text-sm  '
@@ -99,6 +114,11 @@ const UserRegister = (dictionary: Dictionary, isBasicPlan: Boolean) => {
                         >
                             Email:{' '}
                         </Typography>
+                        {errorMailForm && (
+                            <span className='tw-text-red-500'>
+                            Ingrese un correo valido *
+                            </span>
+                        )}
                         <TextField
                             className='tw-h-[52px] tw-mt-1 tw-w-[386px]  tw-text-sm  '
                             required
@@ -108,8 +128,35 @@ const UserRegister = (dictionary: Dictionary, isBasicPlan: Boolean) => {
                             InputProps={{ className: 'tw-rounded-3xl' }}
                             onChange={(e) => setEmail(e.target.value)}
                         />
+                        <Typography
+                            className='tw-text-white  tw-mt-10 tw-w-full'
+                            variant='body2'
+                            color='textSecondary'
+                            display={'flow'}
+                        >
+                            Plan:{' '}
+                        </Typography>
+                        <Select
+                            className='tw-h-[52px] tw-mt-1 tw-w-[386px]  tw-text-sm  '
+                            required
+                            id='outlined-required'
+                            defaultValue={plan}
+                            type='email'
+                            variant='outlined'                            
+                            onChange={(e) => setPlan(e.target.value)}
+                        >
+                            <MenuItem value="basic">Básico</MenuItem>
+                            <MenuItem value="profesional">Profesional</MenuItem>
+                        </Select>
+                        {errorDataForm && (
+                            <span className='tw-text-red-500'>
+                            Complete todos los campos *
+                            </span>
+                        )}
                         
-                        <Button className='tw-text-white tw-ml-36'></Button>
+                        <Button className='tw-text-white tw-mt-10 tw-mb-10 tw-w-[386px] ' onClick={dataRegisterHandle}>
+                            Registrarse
+                        </Button>
                     </Box>
                 </>
             </Container>
