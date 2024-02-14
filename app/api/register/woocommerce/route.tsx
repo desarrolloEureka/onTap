91410fe8edcafc865ed8cb593dbeb4290ade1b70
Manuel Fernández <f20manuel@gmail.com>
2024-02-14 11:26:56 -0500

    resolve plan

--- app/api/register/woocommerce/route.tsx
+++ app/api/register/woocommerce/route.tsx
@@ -1,5 +1,4 @@
 import { registerUserAuth, registerUserFb } from "app/functions/register";
-import { NextResponse } from "next/server";
 
 // payload {"success":"ok","dni":"123456789","email":"email@gmail.com","name":"name","last_name":"lastname"} http://localhost:3000/api/register/post
 export async function POST(request: Request) {
@@ -12,12 +11,14 @@ export async function POST(request: Request) {
     const email = req.billing.email;
     const name = req.billing.first_name;
     const last_name = req.billing.last_name;
-    const plan = req.line_items.map((item: any) => item.name).join(", ");
+    const plan = req.line_items[0];
     if (email && dni && plan) {
       const result = await registerUserAuth({ user: email, password: dni });
+      result.dni = dni;
       result.name = `${name} ${last_name}`;
+      result.email = email;
       result.plan = plan;
-      result.switch_profile = plan === "standard" ? false : true;
+      result.switch_profile = plan.name.toLowerCase().includes("premium");
       const registerResult = await registerUserFb({ data: result });
       response = {
         payload: { dni, email, name, last_name, plan },
