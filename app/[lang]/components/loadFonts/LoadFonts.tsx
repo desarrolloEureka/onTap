fec7e8f729c7022197da69f75db6d9c5aac56da8
camilo-a-g <camilo@eurekadreams.com>
2024-02-13 22:22:52 -0500

    Add FontsTable component and FontsTableLogic hook

--- app/[lang]/components/loadFonts/LoadFonts.tsx
+++ app/[lang]/components/loadFonts/LoadFonts.tsx
@@ -6,6 +6,7 @@ import { Container, Typography } from '@mui/material';
 import { Locale } from 'i18n-config';
 import useDictionary from '@/hooks/dictionary/useDictionary';
 import { Box } from '@mui/system';
+import FontsTable from '../fontsTable/FontsTable';
 
 type Item = {
     id: number;
@@ -22,25 +23,30 @@ const LoadFonts = ({ params: { lang } }: { params: { lang: Locale } }) => {
     };
     return (
         <div className='tw-flex tw-h-screen tw-items-center tw-justify-center tw-bg-[url("/images/loginBackground.png")] tw-bg-no-repeat tw-bg-center tw-bg-cover'>
-            <Container className='tw-bg-[#02AF9B] tw-shadow-m  tw-rounded-2xl tw-h-[700px] tw-w-[800px] tw-flex tw-flex-col tw-justify-center tw-items-center '>
-                <Typography
-                    className='tw-text-white  tw-mt-9 tw-w-full tw-mb-6'
-                    variant='h4'
-                    color='textPrimary'
-                    display={'flow'}
-                    align='center'
-                    fontWeight='bold'
-                >
-                    {dictionary?.backOffice.LoadFonts}
-                </Typography>
-                <Box className='tw-w-[400px] tw-bg-white tw-shadow-m tw-rounded-2xl tw-p-4 tw-mt-4 tw-flex tw-flex-col tw-justify-center tw-items-center '>
-                    <Typography variant='h5' color='textPrimary' display={'flow'} align='center' fontWeight='bold'>
-                        {dictionary?.backOffice.AddDesaign}
-                    </Typography>                    
-                    <ItemForm onAddItem={handleAddItem} dictionary={dictionary} />
-                    <ItemList items={items} />
-                </Box>
-            </Container>
+            <div className='tw-flex tw-flex-col tw-items-center tw-justify-center tw-w-full tw-h-full'>
+                <Container className='tw-bg-[#02AF9B] tw-shadow-m  tw-rounded-2xl tw-h-[500px] tw-w-[600px] tw-flex tw-flex-col tw-justify-center tw-items-center '>
+                    <Typography
+                        className='tw-text-white  tw-mt-9 tw-w-full tw-mb-6'
+                        variant='h4'
+                        color='textPrimary'
+                        display={'flow'}
+                        align='center'
+                        fontWeight='bold'
+                    >
+                        {dictionary?.backOffice.LoadFonts}
+                    </Typography>
+                    <Box className='tw-w-[400px] tw-bg-white tw-shadow-m tw-rounded-2xl tw-p-4 tw-mt-4 tw-flex tw-flex-col tw-justify-center tw-items-center '>
+                        <Typography variant='h5' color='textPrimary' display={'flow'} align='center' fontWeight='bold'>
+                            {dictionary?.backOffice.AddDesaign}
+                        </Typography>
+                        <ItemForm onAddItem={handleAddItem} dictionary={dictionary} />
+                        <ItemList items={items} />
+                    </Box>
+                </Container>
+            </div>
+            <div className='tw-flex tw-flex-col tw-items-center tw-justify-center tw-w-full tw-h-full'>
+                <FontsTable />
+            </div>
         </div>
     );
 };
