dae17d75a29b452048e8662f341b7e0955543162
Manuel Fernández <f20manuel@gmail.com>
2024-02-13 19:01:53 -0500

    test response webhook

--- app/api/register/post/route.tsx
+++ app/api/register/post/route.tsx
@@ -1,10 +1,12 @@
-import { registerUserAuth, registerUserFb } from 'app/functions/register';
+import { registerUserAuth, registerUserFb } from "app/functions/register";
+import { NextResponse } from "next/server";
 
 // payload {"success":"ok","dni":"123456789","email":"email@gmail.com","name":"name","last_name":"lastname"} http://localhost:3000/api/register/post
 export async function POST(request: Request) {
   let response = null;
   try {
     const req = await request.json();
+    return NextResponse.json(req);
     const dni = req.dni;
     const email = req.email;
     const name = req.name;
@@ -14,7 +16,7 @@ export async function POST(request: Request) {
       const result = await registerUserAuth({ user: email, password: dni });
       result.name = `${name} ${last_name}`;
       result.plan = plan;
-      result.switch_profile = plan === 'standard' ? false : true;
+      result.switch_profile = plan === "standard" ? false : true;
       const registerResult = await registerUserFb({ data: result });
       response = {
         payload: { dni, email, name, last_name, plan },
@@ -29,7 +31,7 @@ export async function POST(request: Request) {
     });
   }
 
-  const data = { success: 'ok', response };
+  const data = { success: "ok", response };
   return new Response(JSON.stringify(data), {
     status: 200,
   });
