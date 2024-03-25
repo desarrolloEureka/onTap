e6babbab86fc49b01860b0d7ac94c00f5e7b7f34
manueldev3 <manueldev3@gmail.com>
2024-03-25 18:26:05 -0500

    add status on api

--- app/api/register/post/route.tsx
+++ app/api/register/post/route.tsx
@@ -6,12 +6,13 @@ export async function POST(request: Request) {
   let response = null;
   try {
     const req = await request.json();
+    const status = req.status;
     const dni = req.dni;
     const email = req.email;
     const name = req.name;
     const last_name = req.last_name;
     const plan = req.plan;
-    if (email && dni && plan) {
+    if (email && dni && plan && status === "processing") {
       const result = await registerUserAuth({ user: email, password: dni });
       result.name = `${name} ${last_name}`;
       result.plan = plan;
