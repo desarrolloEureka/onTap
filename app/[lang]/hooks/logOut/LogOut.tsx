ed203fd031fce8583c6c45b33bf8c5a9efb230d6
Diego Peralta <diegoperalta998@gmail.com>
2024-07-26 14:39:18 -0500

    Fix: fix remove storage

--- app/[lang]/hooks/logOut/LogOut.tsx
+++ app/[lang]/hooks/logOut/LogOut.tsx
@@ -1,16 +1,29 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
-const LogOut = () => {
+const useLogOut = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
 
   const logOut = () => {
-    queryClient.removeQueries();
+    // Elimina todas las queries almacenadas
+    queryClient.clear();
+
+    // Limpia localStorage y sessionStorage
     localStorage.clear();
+    sessionStorage.clear();
+
+    // Limpia cookies si es necesario
+    document.cookie.split(";").forEach((cookie) => {
+      const [name] = cookie.split("=");
+      document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+    });
+
+    // Redirigir a la página de login
     router.replace('/views/login');
   };
+
   return { logOut };
 };
 
-export default LogOut;
+export default useLogOut;
