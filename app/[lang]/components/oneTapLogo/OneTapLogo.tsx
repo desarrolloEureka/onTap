40e97106dcc142b593e0433d79f5e65b9f1c95ff
Diego Peralta <diego@eurekadreams.com>
2024-08-30 10:54:54 -0500

    Fix-Logo Link v2

--- app/[lang]/components/oneTapLogo/OneTapLogo.tsx
+++ app/[lang]/components/oneTapLogo/OneTapLogo.tsx
@@ -1,12 +1,10 @@
-
 import Image from 'next/image';
 import Link from 'next/link';
 
-
 const OneTapLogo = () => {
     return (
-        <div className={`tw-flex tw-z-30 tw-h-[8%] tw-items-center tw-justify-center`}>
-            <Link href='https://www.onetap.com.co' target='_blank' className="tw-items-center tw-w-full tw-flex tw-justify-center tw-z-20">
+        <div className="tw-flex tw-z-30 tw-h-[8%] tw-items-center tw-justify-center">
+            <Link href='https://www.onetap.com.co' target='_blank' className="tw-flex tw-justify-center tw-items-center tw-z-20">
                 <Image
                     src='/images/logo_onetap.png'
                     alt="One Tap"
