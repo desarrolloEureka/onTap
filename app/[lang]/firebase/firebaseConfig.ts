a2e2cf58f4fdc05d2d0a5be7e4a93dec2a4b2714
Diego Peralta <diego@eurekadreams.com>
2024-09-16 13:21:17 -0500

    Develop test v2

--- app/[lang]/firebase/firebaseConfig.ts
+++ app/[lang]/firebase/firebaseConfig.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { config as configDotenv } from "dotenv";
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   projectId: process.env.projectId,
@@ -9,6 +10,17 @@ const firebaseConfig = {
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
   measurementId: process.env.measurementId,
+}; */
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  backendBaseUrl: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
 };
 
 // Initialize Firebase
